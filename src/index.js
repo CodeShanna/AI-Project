4884de6f05411c57f8c5d9d8de372424b359b62e
Shanna Jones <shanna@jonesbunch.com>
2025-01-28 13:52:03 +0100

    Updated UI and broke axios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("Preparing a delightful poem for you");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -12,10 +11,16 @@ function generatePoem(event) {
 
   let poemTopic = document.querySelector("#poem-topic");
   let apiKey = "bfaafcoc80td400cfa803cbf6c5ff69f";
-  let context = `You are a whimsical writer that writes poetry for children. Please create a short poem that includes ${poemTopic.value}. Follow "user instructions" and use simple words, 3 or 4 short sentences. Separate every sentence with a <br />Please behave.`;
+  let context = `You are a whimsical writer that writes poetry for children. 
+  Please create a short poem that includes ${poemTopic.value}. Follow "user instructions" and use simple words, 
+  3 or 4 short sentences. Separate every sentence with a <br />Please behave.`;
   let prompt =
     "User Instructions: Please create a short poem to make the end user smile.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = "Creating a home grown poem for you! Please wait....";
 
   axios.get(apiUrl).then(displayPoem);
 }
