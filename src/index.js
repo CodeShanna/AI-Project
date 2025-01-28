d5a39114c7d547ea97b51ab782185c6bff743f44
Shanna Jones <shanna@jonesbunch.com>
2025-01-28 12:33:45 +0100

    Added APIs and AI info

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("Preparing a delightful poem for you");
   new Typewriter("#poem", {
-    strings: "Your poem is being generated.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+function generatePoem(event) {
+  event.preventDefault();
+
+  let poemTopic = document.querySelector("#poem-topic");
+  let apiKey = "bfaafcoc80td400cfa803cbf6c5ff69f";
+  let context = `You are a whimsical writer that writes poetry for children. Please create a short poem that includes ${poemTopic.value}. Follow "user instructions" and use simple words, 3 or 4 short sentences. Separate every sentence with a <br />Please behave.`;
+  let prompt =
+    "User Instructions: Please create a short poem to make the end user smile.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poemForm");
